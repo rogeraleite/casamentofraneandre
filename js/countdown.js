688f0c9ee760bf942263a311f9c108e3815118c9
Roger A. Leite <rogeraleite@gmail.com>
2015-09-03 09:54:20 +0200

    Adding countdown

---
+++ js/countdown.js
@@ -0,0 +1,37 @@
+    CountDownTimer('11/21/2015 04:00 PM', 'countdown');
+    //CountDownTimer('12/20/2015 10:1 AM', 'newcountdown');
+
+    function CountDownTimer(dt, id)
+    {
+        var end = new Date(dt);
+
+        var _second = 1000;
+        var _minute = _second * 60;
+        var _hour = _minute * 60;
+        var _day = _hour * 24;
+        var timer;
+
+        function showRemaining() {
+            var now = new Date();
+            var distance = end - now;
+            if (distance < 0) {
+
+                clearInterval(timer);
+                document.getElementById(id).innerHTML = 'EXPIRED!';
+
+                return;
+            }
+            var days = Math.floor(distance / _day);
+            var hours = Math.floor((distance % _day) / _hour);
+            var minutes = Math.floor((distance % _hour) / _minute);
+            var seconds = Math.floor((distance % _minute) / _second);
+
+            $('#'+id).text(days + ' days ');
+            $('#'+id).append(hours + ' hrs ');
+            $('#'+id).append(minutes + ' mins ');
+            $('#'+id).append(seconds + ' secs');
+        }
+
+        timer = setInterval(showRemaining, 1000);
+    }
+
